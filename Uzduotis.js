39b8ce2d8686c31a9b9b07654b4299b6151ae058
Katkauske <kkatkauske@gmail.com>
2022-10-04 11:36:57 +0300

    JavaScript uzduotys

---
+++ Uzduotis.js
@@ -0,0 +1,25 @@
+console.log("Pirma uzduotis:")
+let x;
+let y;
+x = 2;
+y = 3;
+console.log(x*y)
+
+console.log("Antra uzduotis:")
+let a;
+let b;
+let c;
+a = ("Vienas")
+b = ("Du")
+c = ("Trys")
+console.log(a+b+c)
+
+console.log("Trecia uzduotis:")
+let sk1 = 25;
+let sk2 = 5;
+console.log(sk1/sk2)
+
+console.log("Ketvirta uzduotis:")
+let d = 2;
+let e = 2;
+alert(d**e)
