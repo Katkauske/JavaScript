dfaff69f159766ff398f1493a85351cae36dbe85
dovydasvaliokas <74957396+dovydasvaliokas@users.noreply.github.com>
2022-10-05 22:40:22 +0300

    Turėjo būti ne skaičiai, o tekstiniai kintamieji

--- Uzduotis2.js
+++ Uzduotis2.js
@@ -2,7 +2,7 @@ console.log("Antra uzduotis. Sudetis:")
 let a;
 let b;
 let c;
-a = +prompt("Ivesti pirmaji skaiciu:")
-b = +prompt("Ivesti antraji skaiciu:")
-c = +prompt("Ivesti treciaji skaiciu:")
+a = prompt("Ivesti pirmaji skaiciu:")         // pliuso nereikia, kadangi užduotyje prašo sudėti tris TEKSTINIUS kintamuosius
+b = prompt("Ivesti antraji skaiciu:")         // pliuso nereikia, kadangi užduotyje prašo sudėti tris TEKSTINIUS kintamuosius
+c = prompt("Ivesti treciaji skaiciu:")        // pliuso nereikia, kadangi užduotyje prašo sudėti tris TEKSTINIUS kintamuosius
 console.log(a+b+c)
